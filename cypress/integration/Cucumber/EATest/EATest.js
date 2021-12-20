4fbcf867092d688305436bb53b12a83d7ad89f58
Shikhar Joshi <shikhar.joshi@qualitestgroup.com>
2021-12-20 17:06:26 +0530

    Modified the code as per App Action Pattern

--- cypress/integration/Cucumber/EATest/EATest.js
+++ cypress/integration/Cucumber/EATest/EATest.js
@@ -1,4 +1,5 @@
-import { Given } from "cypress-cucumber-preprocessor/steps";
+import { And, Given } from "cypress-cucumber-preprocessor/steps";
+import {loginPage} from "../../my-own-tests/pages/ealoginpage";
 
 Given(`I visit EA site`, () => {
   cy.visit("http://eaapp.somee.com/");
@@ -9,11 +10,13 @@ Given(`I click login link`, () => {
 });
 
 And(`I login as following`, (dataTable) => {
+  dataTable.hashes().forEach(row => {
+    // cy.get('#UserName').type(row.userName);
+    // //extra param to hide password in log while running in UI
+    // cy.get('#Password').type(row.Password, { log: false });
 
-    dataTable.hashes().forEach(row => {
-        cy.get('#UserName').type(row.userName);
-        //extra param to hide password in log while running in CI
-        cy.get('#Password').type(row.Password, { log: false });
-    })
-    cy.get(".btn").click();
+    loginPage.performLogin(row.userName, row.Password)
+  })
+  // cy.get(".btn").click();
+  loginPage.clickLoginButton()
 });
